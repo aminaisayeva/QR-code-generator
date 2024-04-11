e16ae6ec6bd151648567cb9444025db07addaefc
Amina Isayeva <105892147+aminaisayeva@users.noreply.github.com>
2024-04-11 19:15:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,16 +9,16 @@ inquirer.prompt([
     message: 'What URL would you like to generate a QR code for?',
   },
 ]).then(answers => {
-  // Generate QR Code
+  // generate qr Code
   const qrCode = qr.image(answers.url, { type: 'png' });
   
-  // Create a writable stream to a file
+  // writable stream to a file
   const output = fs.createWriteStream('qr_img.png');
   
-  // Pipe the QR Code to the file
+  // forward qr Code to  file
   qrCode.pipe(output);
   
-  // Save the URL in a text file
+  // save  URL in a txt file
   fs.writeFile('URL.txt', answers.url, err => {
     if(err) {
       console.error('Error writing to file', err);
